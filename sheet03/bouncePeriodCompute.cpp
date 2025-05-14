c2039587075c65f63c472ef5622da6a2508a8897
Ashwin Balaji <s.ashwinbalaji.de@gmail.com>
2025-05-14 16:41:18 +0200

    first part done???

--- sheet03/bouncePeriodCompute.cpp
+++ sheet03/bouncePeriodCompute.cpp
@@ -19,12 +19,13 @@ double B(double z) {
 }
 
 double v_parallel(double z) {
-    return sqrt(2 * (E - mu * B(z)) / m);
+    double term = 2 * (E - mu * B(z)) / m;
+    return term > 0 ? sqrt(term) : 0;
 }
 
 double integrand(double z) {
     double v_par = v_parallel(z);
-    return 1 / (v_par + 1e-12);
+    return v_par > 0 ? 1 / v_par : 0;
 }
 
 double trapezoidalRule(double a, double b, int n) {
@@ -60,27 +61,40 @@ int main() {
     double b = 0.1875;
     double EPS = 1e-3;
     double v1_trap, v2_trap, v1_simp, v2_simp;
-    int n = 10;
+    int n_trap = 10;
+    int n_simp = 10;
 
     std::ofstream outFile("bouncePeriodCompute.txt");
 
-    v1_trap = trapezoidalRule(a, b, n);
-    v1_simp = simpsonsRule(a, b, n);
-
-    std::cout << "Iteration " << 0 << ", Trap: " << std::setprecision(10) << v1_trap << ", Simpson: " << v1_simp << std::endl;
-    outFile << "Iteration " << 0 << ", Trap: " << std::setprecision(10) << v1_trap << ", Simpson: " << v1_simp << std::endl;
+    v1_trap = trapezoidalRule(a, b, n_trap);
+    std::cout << "Iteration " << 0 << ", Trap: " << std::setprecision(10) << v1_trap << std::endl;
+    outFile << "Iteration " << 0 << ", Trap: " << std::setprecision(10) << v1_trap << std::endl;
 
     int i = 1;
     do {
-        n *= 2;
-        v2_trap = trapezoidalRule(a, b, n);
-        v2_simp = simpsonsRule(a, b, n);
+        n_trap *= 2;
+        v2_trap = trapezoidalRule(a, b, n_trap);
 
-        std::cout << "Iteration " << i << ", Trap: " << std::setprecision(10) << v2_trap << ", Simpson: " << v2_simp << std::endl;
-        outFile << "Iteration " << i << ", Trap: " << std::setprecision(10) << v2_trap << ", Simpson: " << v2_simp << std::endl;
+        std::cout << "Iteration " << i << ", Trap: " << std::setprecision(10) << v2_trap << std::endl;
+        outFile << "Iteration " << i << ", Trap: " << std::setprecision(10) << v2_trap << std::endl;
 
         i++;
-    } while (fabs(v2_trap - v1_trap) >= EPS || fabs(v2_simp - v1_simp) >= EPS);
+    } while (fabs(v2_trap - v1_trap) >= EPS);
+
+    v1_simp = simpsonsRule(a, b, n_simp);
+    std::cout << "Iteration " << 0 << ", Simpson: " << std::setprecision(10) << v1_simp << std::endl;
+    outFile << "Iteration " << 0 << ", Simpson: " << std::setprecision(10) << v1_simp << std::endl;
+
+    int j = 1;
+    do {
+        n_simp *= 2;
+        v2_simp = simpsonsRule(a, b, n_simp);
+
+        std::cout << "Iteration " << j << ", Simpson: " << std::setprecision(10) << v2_simp << std::endl;
+        outFile << "Iteration " << j << ", Simpson: " << std::setprecision(10) << v2_simp << std::endl;
+
+        j++;
+    } while (fabs(v2_simp - v1_simp) >= EPS);
 
     outFile.close();
 
